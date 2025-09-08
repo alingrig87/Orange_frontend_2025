dce816b8afca2309f32ccb735489ecf9dc5d31aa
Alin Grigorescu <alin.grig87@gmail.com>
2025-09-08 19:34:31 +0300

    js - part3

--- 08_JS_Basics/07_loops/app.js
+++ 08_JS_Basics/07_loops/app.js
@@ -98,3 +98,20 @@ while (i <= toateMasinile.length - 1) {
 	console.log(toateMasinile[i]);
 	i++;
 }
+
+var i = 1;
+while (i <= 10) {
+	console.log(i);
+	i++;
+}
+
+for (var i = 1; i <= 10; i++) {
+	console.log(i);
+}
+
+var nums = [2, 1, 3, 5, 0, 4, 8];
+
+// parcurgere array
+for (var i = 0; i <= nums.length - 1; i++) {
+	console.log(nums[i]);
+}
